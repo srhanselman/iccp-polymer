e43b6af9fc89992700d1fb5d65c3640bc64a34da
lenniederoo <lenniederoo@hotmail.com>
2015-03-26 13:16:48 +0000

    working potcalc door josmagie

--- Potcalc.f90
+++ Potcalc.f90
@@ -5,18 +5,18 @@ subroutine calc_pot(pos,optpos,pot,arraylength,N,Nopt)
   integer, intent(in) :: arraylength
   real(8), intent(in) :: pos(arraylength, 3)
   integer, intent(in) :: Nopt
-  real(8), intent(inout) :: pot(Nopt,1)
+  real(8), intent(inout) :: pot(Nopt)
   !f2py intent(in, out) pot
   real(8), intent(in) :: optpos(Nopt, 3)
   real(8) :: delta_r(3), dr2
   real(8), parameter :: rmax = 3.2_8
   integer :: i, j
-  do i = 1, N
-  pot(i,1) = 0.0
-    do j=1,Nopt
+  do i = 1, Nopt
+  pot(i) = 0.0
+    do j=1,N
       delta_r = optpos(i,:) - pos(j,:)
       dr2 = sum(delta_r**2)
-      pot(i,1)=pot(i,1)+4*((1/dr2)**6 - (1/dr2)**3)
+      pot(i)=pot(i)+4*((1/dr2)**6 - (1/dr2)**3) 
     end do
   end do
 end subroutine

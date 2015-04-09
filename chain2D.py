Trashcontainer deleted, useless variable deleted, del used instead of remove, stupid sum is now average, however strange bug has come

chain2D.py
@@ -22,7 +22,7 @@ class chain:
     pot=np.zeros((num_options,1),dtype=float)
     pot=self.calc_pot(pot,options,num_options)
     w=np.exp(-pot) #weights
-    if all(w==0):
+    if all(w==0.0):
       self.stop_chain()
       return
     i,p,W=self.weighted_random_choice(num_options,w)
@@ -52,15 +52,19 @@ class chain:
     
   def weighted_random_choice(self,num_options,w):
     W = sum(w)
+    if W==0.0:
+      print 'W',W
+      print  'w',w
     pick = np.random.uniform(0, W)
     current = 0
     for i in xrange(0,num_options):
         current += w[i]
         if current > pick:
+            #print 'i',i,'w[i]',w[i],'W',W
             return i,w[i]/W,W
             
   def stop_chain(self):
       self.prob=0
       self.Bool=False
       self.weight=0
-      #print 'I am a hopeless chain! :('
+      print 'I am a hopeless chain! :('

listsofchains2D.py
@@ -2,19 +2,17 @@ import numpy as np
 import copy
 from chain2D import chain
 class Active_Chains:
-  def __init__(self,numberofchains,numberofparticles,max_num_part,num_options):
+  def __init__(self,numberofchains,numberofparticles,num_options):
     self.List=list()
-    self.container=CONTAINER()
     self.num_chains=0
-    self.num_part_added=numberofparticles-2
-    self.max_num_part=max_num_part
+    self.numberofparticles=numberofparticles
     self.num_options=num_options
     self.init_list(numberofchains)
     self.weight3=self.calc_w3()
     
   def init_list(self,numberofchains):
     for i in xrange(0,numberofchains):
-      self.add_chain(chain(self.max_num_part))
+      self.add_chain(chain(self.numberofparticles))
       
   def show(self):
     print "List of Active Chains:"
@@ -26,23 +24,22 @@ class Active_Chains:
       print "weight:",i.weight
       print "probability:",i.prob
       print "Boolean:",i.Bool
-    print "Trash :",len(self.container.trash)
   
   def add_chain(self,chain):
     self.List.append(chain)
     self.num_chains+=1    
     
   def run(self):
-    for j in xrange(0,self.num_part_added):
+    for j in xrange(0,self.numberofparticles-2):
       for i in self.List[:]:
         if i.Bool==True:
           i.add_particle(self.num_options)
-      self.prune()
+      self.prune2()
 
   def prune(self):
-    Avprob=np.sum(chain.weight for chain in self.List)
-    UpLim=2.0*Avprob/self.weight3
-    LowLim=1.2*Avprob/self.weight3
+    Avweight=np.sum(chain.weight for chain in self.List)/self.num_chains
+    UpLim=2.0*Avweight/self.weight3
+    LowLim=1.2*Avweight/self.weight3
     for i in self.List[:]:
       if i.weight>UpLim:
         i.weight=i.weight*0.5
@@ -57,20 +54,30 @@ class Active_Chains:
           else:
             i.weight=i.weight*2                     
     
+  def prune2(self):
+    Avweight=np.sum(chain.weight for chain in self.List)/len(self.List)
+    UpLim=2.0*Avweight/self.weight3
+    LowLim=1.2*Avweight/self.weight3
+    for i in xrange(len(self.List) - 1, -1, -1): #looping backwards
+      if self.List[i].weight>UpLim:
+        self.List[i].weight=self.List[i].weight*0.5
+        self.add_chain(copy.deepcopy(self.List[i])) #python does weird referencings!
+        #print ":D"
+      else:
+        if self.List[i].weight<=LowLim:
+          rand=np.random.uniform(0.0,1.0,size=1)
+          if rand<0.5:
+           del self.List[i]
+            #print ":("
+          else:
+              self.List[i].weight=self.List[i].weight*2      
+              
   def calc_w3(self):
     w3chain=chain(3)
     w3chain.add_particle(self.num_options)
     return w3chain.weight  
     
   def trash(self,chain):
-    self.container.add_trash(chain)
     self.List.remove(chain)
     self.num_chains-=1
     
-
-class CONTAINER:
-  def __init__(self):
-    self.trash=list()
-  def add_trash(self,chain):
-      self.trash.append(chain)
-      